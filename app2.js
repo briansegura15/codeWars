7355a7c290aed073c2410b4bf8541a646fa59090
briansegura15 <84042284+briansegura15@users.noreply.github.com>
2022-09-27 16:28:30 -0400

    delete!

--- app2.js
+++
@@ -1,9 +0,0 @@
-function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
-  // code goes here
-
-  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
-}
-
-// Alternate solution
-
-const goals = (...a) => a.reduce((s, v) => s + v, 0);
