b38d7b3c955f74e940346590bd9c878a683752ee
briansegura15 <84042284+briansegura15@users.noreply.github.com>
2022-09-26 22:07:39 -0400

    level 8 kyu complete today

---
+++ 2022-9-26/app.js
@@ -0,0 +1,10 @@
+function numberToString(num) {
+  // toString is a method to convert to string.
+  return num.toString();
+}
+
+// Alternate solution
+function numberToString(num) {
+  // Return a string of the number here!
+  return String(num);
+}
