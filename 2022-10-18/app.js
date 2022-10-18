23f81cb54fc7db850e68bfce9343bb3a033781b3
briansegura15 <84042284+briansegura15@users.noreply.github.com>
2022-10-18 09:58:10 -0400

    first challenge done for the day

---
+++ 2022-10-18/app.js
@@ -0,0 +1,8 @@
+function getAge(inputString) {
+  // return the girl's correct age as an integer. Happy coding :)
+  // parseInto turns parameter into a string
+  return parseInt(inputString);
+}
+
+// alternate solutions
+const getAge = parseInt;
