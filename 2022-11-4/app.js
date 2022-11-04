20b4f1692015af9214fa63f69ccbffd3d1b07d77
briansegura15 <84042284+briansegura15@users.noreply.github.com>
2022-11-04 12:27:14 -0400

    first challenge of the day complete

---
+++ 2022-11-4/app.js
@@ -0,0 +1,23 @@
+function finalGrade(exam, projects) {
+  if (exam > 90 || projects > 10) {
+    return 100;
+  } else if (exam > 75 && projects >= 5) {
+    return 90;
+  } else if (exam > 50 && projects >= 2) {
+    return 75;
+  } else {
+    return 0;
+  }
+}
+
+// alternate solution
+
+const finalGrade = (exam, projects) => {
+  return exam > 90 || projects > 10
+    ? 100
+    : exam > 75 && projects >= 5
+    ? 90
+    : exam > 50 && projects >= 2
+    ? 75
+    : 0;
+};
