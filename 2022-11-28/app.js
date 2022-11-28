14d2694786cf4d77b87a092a51a7d0eb7d920f2b
briansegura15 <84042284+briansegura15@users.noreply.github.com>
2022-11-28 13:47:04 -0500

    challenge of the day complete

---
+++ 2022-11-28/app.js
@@ -0,0 +1,18 @@
+function isIsogram(str) {
+  //...
+  str = str.toLowerCase();
+  for (let i = 0; i < str.length; i++) {
+    for (let j = i + 1; j < str.length; j++) {
+      if (str[i] === str[j]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+// alternate solution
+
+function isIsogram(str) {
+  return new Set(str.toUpperCase()).size == str.length;
+}
