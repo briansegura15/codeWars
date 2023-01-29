a3a161c5c6f3982332ea0372a70513f1af7aff4f
briansegura15 <84042284+briansegura15@users.noreply.github.com>
2023-01-29 14:27:16 -0500

    daily challenge complete

---
+++ 2023-1-29/app.js
@@ -0,0 +1,11 @@
+var summation = function (num) {
+  let sum = 0;
+  for (let i = 0; i <= num; i++) {
+    sum += i;
+  }
+  return sum;
+};
+
+// Alternate solution aka (Gauss Formula)
+
+const summation = n => (n * (n + 1)) / 2;
