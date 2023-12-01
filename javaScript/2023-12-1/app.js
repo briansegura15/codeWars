84cc900e25c6239de26b58c1ef904735d6662102
briansegura15 <84042284+briansegura15@users.noreply.github.com>
2023-12-01 12:39:50 -0500

    hello diciembre, let us see what gifts you bring us this year

---
+++ javaScript/2023-12-1/app.js
@@ -0,0 +1,16 @@
+function divisibleBy(numbers, divisor) {
+  let result = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % divisor === 0) {
+      result.push(numbers[i]);
+    }
+  }
+  return result;
+}
+
+//   alternate solution
+
+function divisibleBy(numbers, divisor) {
+  return numbers.filter(n => n % divisor === 0);
+}
