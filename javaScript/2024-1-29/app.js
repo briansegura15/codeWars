073d88eddc841d8f354833f91ea93bb5d42653a6
briansegura15 <84042284+briansegura15@users.noreply.github.com>
2024-01-29 19:54:14 -0500

    first leetcode problem complete with Maye and Judy, they rock, lets keep going.

---
+++ javaScript/2024-1-29/app.js
@@ -0,0 +1,13 @@
+var containsDuplicate = function (nums) {
+  //   let sortedNums = nums.sort((a, b) => a - b);
+  let set = new Set(nums);
+  console.log(set);
+  return set.size === nums.length;
+};
+
+[1, 2, 3, 1];
+console.log(containsDuplicate([1, 1, 3, 4, 2]));
+
+// some sort
+// index lastindexof
+// use Set because its a super power use new
