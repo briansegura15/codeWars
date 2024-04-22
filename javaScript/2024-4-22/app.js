66ec278e50e4cc322e648b7f4bde84bcd2eb7117
briansegura15 <84042284+briansegura15@users.noreply.github.com>
2024-04-22 16:31:32 -0400

    daily challenge complete

---
+++ javaScript/2024-4-22/app.js
@@ -0,0 +1,14 @@
+class Solution {
+  static main() {
+    console.log("Hello World!");
+  }
+}
+
+// alternate
+
+class Sol {
+  main() {
+    console.log("Hello World!");
+  }
+}
+var Solution = new Sol();
